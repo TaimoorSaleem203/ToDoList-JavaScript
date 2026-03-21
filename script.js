05f1b631974a0fab60f6f2f299659ccca7aca6df
Taimoor <taimoorsaleem2005@gmail.com>
2026-03-21 12:43:34 +0500

    init

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
+const taskInp = document.querySelector(".todo-input");
+const container = document.querySelector(".section-container");
+const taskList = document.querySelector(".todo-list");
 
-const taskInp = document.querySelector(".todo-input")
-const addBtn = document.querySelector(".add-btn")
-const container = document.querySelector(".section-container")
-const taskList = document.querySelector(".todo-list")
-
-let result = []
+const addBtn = document.querySelector(".add-btn");
+const deleteBtn = document.querySelector(".delete-icon");
 
 const setLocalStorage = (result) => {
-    localStorage.setItem("task",JSON.stringify(result))
-}
+  localStorage.setItem("task", JSON.stringify(result));
+};
 
 const getLocalStorage = () => {
-    JSON.parse(localStorage.getItem("task"))
-}
+  return JSON.parse(localStorage.getItem("task")) || [];
+};
+
+let result = [];
+
+const showTask = () => {
+  let tasks = getLocalStorage() || result;
+
+  const taskItems = document.createElement("li");
+  taskItems.classList.add("todo-items");
+
+  tasks.forEach((item, indx) => {
+    taskItems.innerHTML = `${indx + 1}. ${item.task} <i class="ri-delete-bin-5-fill delete-icon"></i>`;
+  });
+
+  taskList.appendChild(taskItems);
+};
 
 const addTask = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    if(taskInp.value=="") return 
+  if (taskInp.value == "") return;
 
-    let updatedTask = {"task":taskInp.value,"date":Date.now()}
-    result.push(updatedTask)
+  let updatedTask = { task: taskInp.value, date: Date.now() };
+  result.push(updatedTask);
+  setLocalStorage(result);
 
-    const taskItems = document.createElement("li")
-    taskItems.classList.add("todo-items")
+  showTask();
 
-    result.forEach((item,indx)=>{
-        taskItems.innerText = `${indx+1}. ${item.task}`
-    })
+  taskInp.value = "";
+};
 
-    taskList.appendChild(taskItems)
-    taskInp.value = ""
-}
+const deleteTask = (index) => {
+    result.splice(index,1)
+    showTask()
+};
 
-addBtn.addEventListener("click",addTask)
-taskInp.addEventListener("keydown",(e)=>{
-    if(e.key=="Enter"){
-        e.preventDefault();
-        addTask();
-    }
-})
+addBtn.addEventListener("click", addTask);
+taskInp.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+    addTask(e);
+  }
+});
