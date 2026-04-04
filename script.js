954454f3b054255a1fa3b212e2b357f4775e02e9
Taimoor <taimoorsaleem2005@gmail.com>
2026-04-04 22:54:59 +0500

    CRUD logic

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const taskInp = document.querySelector(".todo-input");
-const taskSearch = document.querySelector(".todo-search");
+const searchBar = document.querySelector(".todo-search");
 const taskList = document.querySelector(".todo-list");
-const searchBar = document.querySelector(".searchbar")
 const addBtn = document.querySelector(".add-btn");
-const deleteBtn = document.querySelector(".delete-icon")
 
 let todo = []
+let editingIndex = null
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault()
@@ -14,10 +13,38 @@ addBtn.addEventListener("click", (e) => {
   addTask()
 })
 
+searchBar.addEventListener("input",(e)=>{
+
+  const value = e.target.value.toLowerCase()
+  const filtered = todo.filter((item,index)=>item.task.toLowerCase().includes(value))
+
+  displayTask(filtered)
+})
+
+
 taskList.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-icon")) {
+
     const indx = e.target.parentElement.getAttribute("data-id")
-    deleteTask(indx)
+    if (editingIndex == null) {
+      deleteTask(indx)       
+    }
+                   
+  } else if (e.target.classList.contains("edit-icon")) {
+
+    const indx = e.target.parentElement.getAttribute("data-id")
+    taskInp.value = todo[indx].task
+    addBtn.textContent = "Update"
+    editingIndex = indx
+
+  }else if (e.target.classList.contains("check-icon")){
+
+    const taskEl = e.target.closest(".todo-items")
+    const text = taskEl.querySelector(".todo-text")  
+    const indx = e.target.getAttribute("data-id")
+    
+    todo[indx].completed = !todo[indx].completed
+    text.classList.toggle("completed")
   }
 })
 
@@ -30,34 +57,49 @@ function getStorage() {
   if (saved != null) todo = JSON.parse(saved)
 }
 
-function displayTask() {
+function displayTask(todo) {
   taskList.innerHTML = "" // Avoid Duplication , Clears Previous UI
 
   todo.map((item, indx) => {
     const li = document.createElement("li")
     li.className = "todo-items"
 
-    li.innerHTML = `${item.task}<br/><button data-id="${indx}"><i class="ri-close-line delete-icon"></i></button>`
-    
+    li.innerHTML = `
+                    <div class="todo-pending"></div>
+                    <p class="todo-text">${item.task}</p>
+                    <input data-id="${indx}" type="checkbox" class="check-icon" />
+                    <button data-id="${indx}"><i class="ri-close-line delete-icon"></i></button>
+                    <button data-id="${indx}"><i class="ri-edit-line edit-icon"></i></button>`
+
     taskList.appendChild(li)
   })
 }
 
 function addTask() {
-  todo.push({ task: taskInp.value, date: Date.now() })
+
+  if (editingIndex != null) {
+
+    todo[editingIndex].task = taskInp.value
+    addBtn.textContent = "Add"
+    editingIndex = null
+
+  } else {
+    todo.push({ task: taskInp.value, date: Date.now(), completed:false })
+  }
+
   taskInp.value = ""
 
   addStorage()
-  displayTask()
+  displayTask(todo)
 }
 
 function deleteTask(indx) {
+  // At a specific indx remove 1 item
   todo.splice(indx, 1)
 
   addStorage()
-  displayTask()
+  displayTask(todo)
 }
 
-
 getStorage()
-displayTask()
+displayTask(todo)
